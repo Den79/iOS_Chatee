7e9dd2955285db3187c20b28ffc567687a130762
Denis M <den@deniss-mac-mini.wifi.bcit.ca>
2020-02-24 10:02:50 -0800

    Pods were installed

---
+++ Chatee/Chatee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatee
+//
+//  Created by Denis M on 2020-02-24.
+//  Copyright © 2020 Denis M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
